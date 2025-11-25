148a89b3322017c6ef76466703b38f068834e27f
ben <nabhanggt@gmail.com>
2025-11-25 18:34:22 +0700

    Fix: Kembalikan ke \->faker dengan model binding di ArticleFactory

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,14 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\Article;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<Article>
  */
 class ArticleFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Article::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,21 +25,20 @@ class ArticleFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = $this->faker ?? \Faker\Factory::create();
-        $title = $faker->unique()->sentence(5);
-        $published = $faker->boolean(80);
+        $title = $this->faker->unique()->sentence(5);
+        $published = $this->faker->boolean(80);
 
         return [
             'title' => $title,
             'slug' => Str::slug($title),
-            'author' => $faker->name(),
-            'summary' => $faker->paragraph(),
+            'author' => $this->faker->name(),
+            'summary' => $this->faker->paragraph(),
             'content' => collect(range(1, 5))
-                ->map(fn () => '<p>'.$faker->paragraph(6).'</p>')
+                ->map(fn () => '<p>'.$this->faker->paragraph(6).'</p>')
                 ->implode("\n"),
             'thumbnail_path' => null,
             'is_published' => $published,
-            'published_at' => $published ? $faker->dateTimeBetween('-2 years') : null,
+            'published_at' => $published ? $this->faker->dateTimeBetween('-2 years') : null,
         ];
     }
 }
